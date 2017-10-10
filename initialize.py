89c16c4f6f599e0875b3a45dfc1104763ce79906
Juergen Hackl <hackl@ibi.baug.ethz.ch>
2017-10-10 08:23:04 +0200

    add function write_shp

--- initialize.py
+++ initialize.py
@@ -3,7 +3,7 @@
 # =============================================================================
 # File      : initialize.py 
 # Creation  : 26 Oct 2015
-# Time-stamp: <Mit 2017-06-14 15:22 juergen>
+# Time-stamp: <Die 2017-10-10 08:18 juergen>
 #
 # Copyright (c) 2015 Jürgen Hackl <hackl@ibi.baug.ethz.ch>
 #               http://www.ibi.ethz.ch
@@ -93,6 +93,136 @@ def read_shp(path):
                 net.add_edge(u_node, v_node, attributes)
 
     return net
+
+
+def write_shp(G, outdir, filename):
+    """Writes a networkx.DiGraph to two shapefiles, edges and nodes.
+    Nodes and edges are expected to have a Well Known Binary (Wkb) or
+    Well Known Text (Wkt) key in order to generate geometries. Also
+    acceptable are nodes with a numeric tuple key (x,y).
+
+    "The Esri Shapefile or simply a shapefile is a popular geospatial vector
+    data format for geographic information systems software [1]_."
+
+    Parameters
+    ----------
+    outdir : directory path
+       Output directory for the two shapefiles.
+
+    Returns
+    -------
+    None
+
+    Examples
+    --------
+    nx.write_shp(digraph, '/shapefiles') # doctest +SKIP
+
+    References
+    ----------
+    .. [1] http://en.wikipedia.org/wiki/Shapefile
+    """
+    try:
+        from osgeo import ogr
+    except ImportError:
+        raise ImportError("write_shp requires OGR: http://www.gdal.org/")
+    # easier to debug in python if ogr throws exceptions
+    ogr.UseExceptions()
+
+    def netgeometry(key, data):
+        if 'Wkb' in data:
+            geom = ogr.CreateGeometryFromWkb(data['Wkb'])
+        elif 'Wkt' in data:
+            geom = ogr.CreateGeometryFromWkt(data['Wkt'])
+        elif type(key[0]).__name__ == 'tuple':  # edge keys are packed tuples
+            geom = ogr.Geometry(ogr.wkbLineString)
+            _from, _to = key[0], key[1]
+            try:
+                geom.SetPoint(0, *_from)
+                geom.SetPoint(1, *_to)
+            except TypeError:
+                # assume user used tuple of int and choked ogr
+                _ffrom = [float(x) for x in _from]
+                _fto = [float(x) for x in _to]
+                geom.SetPoint(0, *_ffrom)
+                geom.SetPoint(1, *_fto)
+        else:
+            geom = ogr.Geometry(ogr.wkbPoint)
+            try:
+                geom.SetPoint(0, *key)
+            except TypeError:
+                # assume user used tuple of int and choked ogr
+                fkey = [float(x) for x in key]
+                geom.SetPoint(0, *fkey)
+
+        return geom
+
+    # Create_feature with new optional attributes arg (should be dict type)
+    def create_feature(geometry, lyr, attributes=None):
+        feature = ogr.Feature(lyr.GetLayerDefn())
+        feature.SetGeometry(g)
+        if attributes != None:
+            # Loop through attributes, assigning data to each field
+            for field, data in attributes.items():
+                feature.SetField(field, data)
+        lyr.CreateFeature(feature)
+        feature.Destroy()
+
+    drv = ogr.GetDriverByName("ESRI Shapefile")
+    shpdir = drv.CreateDataSource(outdir)
+    # delete pre-existing output first otherwise ogr chokes
+    try:
+        shpdir.DeleteLayer(filename + '_n')
+    except:
+        pass
+    nodes = shpdir.CreateLayer(filename + '_n', None, ogr.wkbPoint)
+    for n in G:
+        data = G.node[n]
+        g = netgeometry(n, data)
+        create_feature(g, nodes)
+    try:
+        shpdir.DeleteLayer(filename + '_e')
+    except:
+        pass
+    edges = shpdir.CreateLayer(filename + '_e', None, ogr.wkbLineString)
+
+    # New edge attribute write support merged into edge loop
+    fields = {}      # storage for field names and their data types
+    attributes = {}  # storage for attribute data (indexed by field names)
+
+    # Conversion dict between python and ogr types
+    OGRTypes = {int: ogr.OFTInteger, str: ogr.OFTString, float: ogr.OFTReal}
+
+    # Edge loop
+    for e in G.edges(data=True):
+        data = G.get_edge_data(*e)
+        g = netgeometry(e, data)
+        # Loop through attribute data in edges
+        for key, data in e[2].items():
+            # Reject spatial data not required for attribute table
+            if (key != 'Json' and key != 'Wkt' and key != 'Wkb'
+                and key != 'ShpName'):
+                  # For all edges check/add field and data type to fields dict
+                    if key not in fields:
+                  # Field not in previous edges so add to dict
+                        if type(data) in OGRTypes:
+                            fields[key] = OGRTypes[type(data)]
+                        else:
+                            # Data type not supported, default to string (char 80)
+                            fields[key] = ogr.OFTString
+                        # Create the new field
+                        newfield = ogr.FieldDefn(key, fields[key])
+                        edges.CreateField(newfield)
+                        # Store the data from new field to dict for CreateLayer()
+                        attributes[key] = data
+                    else:
+                     # Field already exists, add data to dict for CreateLayer()
+                        attributes[key] = data
+        # Create the feature with, passing new attribute data
+        create_feature(g, edges, attributes)
+
+    nodes, edges = None, None
+
+
 def remove_oneways(graph):
     for edge in graph.edges():
         u = edge[0]
